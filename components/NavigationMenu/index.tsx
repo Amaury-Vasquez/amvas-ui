a0bc22a880d08b7f96e091484e27236ae3a2dcb7
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-29 21:40:24 -0600

    create navigation menu component

---
+++ components/NavigationMenu/index.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import clsx from 'clsx';
+import { NavigationLink, NavigationLinkProps } from 'components';
+
+interface NavigationMenuProps {
+  className?: string;
+  links: NavigationLinkProps[];
+  animation?: string;
+  column?: boolean;
+}
+
+const NavigationMenu: FC<NavigationMenuProps> = ({
+  className,
+  links,
+  animation,
+  column = true,
+}) => {
+  return (
+    <div
+      className={clsx(
+        'w-full h-auto capitalize flex items-center justify-start',
+        column ? 'flex-col' : 'flex-row',
+        animation,
+        className
+      )}
+    >
+      {links.map((link, i) => (
+        <NavigationLink
+          className={
+            'h-full w-full px-4 py-2 bg-violet-800 text-base font-semibold border-b border-violet-600'
+          }
+          key={i}
+          {...link}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default NavigationMenu;
