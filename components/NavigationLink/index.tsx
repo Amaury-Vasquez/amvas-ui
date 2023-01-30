342a5463bb472eecf5dc1b6499d63b913bc24a52
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-29 21:38:27 -0600

    create navigation link component

---
+++ components/NavigationLink/index.tsx
@@ -0,0 +1,46 @@
+import { FC } from 'react';
+import { IconType } from 'react-icons';
+import Link from 'next/link';
+import clsx from 'clsx';
+
+export interface NavigationLinkProps {
+  text: string;
+  className?: string;
+  href?: string;
+  Icon?: IconType;
+  iconRight?: boolean;
+}
+
+const NavigationLink: FC<NavigationLinkProps> = ({
+  text,
+  href = '/',
+  Icon,
+  iconRight,
+  className,
+}) => {
+  const textStyle = clsx(
+    'flex items-center justify-center gap-2 text-white text-center',
+    className
+  );
+
+  if (Icon) {
+    return iconRight ? (
+      <Link href={href} className={textStyle}>
+        {text}
+        <Icon />
+      </Link>
+    ) : (
+      <Link href={href} className={textStyle}>
+        <Icon />
+        {text}
+      </Link>
+    );
+  }
+  return (
+    <Link href={href} className={textStyle}>
+      {text}
+    </Link>
+  );
+};
+
+export default NavigationLink;
