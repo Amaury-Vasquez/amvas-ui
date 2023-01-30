058a58cfdcf115ed3d8ed9b58960cc1bd3ec4ac3
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-29 21:34:59 -0600

    add scroll down animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Note the addition of the `app` directory.
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'scroll-down': {
+          '0%': { transform: 'translateY(-100%)' },
+          '70%': { transform: 'translateY(8px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        'to-bottom': 'scroll-down 1s ease-in-out',
+      },
+    },
   },
   plugins: [],
 };
