90096c24071548ac5d8d4bcde3b9cfe80b2d8d83
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-31 21:15:13 -0600

    update exports

--- components/index.tsx
+++ components/index.tsx
@@ -1,8 +1,11 @@
+export { default as Accordion } from './Accordion';
+export { default as Article } from './Article';
+export { default as Anchor } from './Anchor';
 export { default as Layout } from './Layout';
-export { default as Sider } from './Sider';
 export {
   default as NavigationLink,
   type NavigationLinkProps,
 } from './NavigationLink';
 export { default as NavigationMenu } from './NavigationMenu';
-export { default as Accordion } from './Accordion';
+export { default as Separator } from './Separator';
+export { default as Sider } from './Sider';
