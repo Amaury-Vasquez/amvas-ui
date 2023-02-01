dec37c15e69029b3ebabd3e51cc5cbe904732fc7
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-01-31 21:17:32 -0600

    change quotes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
-import type { AppProps } from "next/app";
-import { Source_Sans_Pro } from "@next/font/google";
-import { Layout } from "components";
-import "tailwindcss/tailwind.css";
-import "styles/globals.css";
+import type { AppProps } from 'next/app';
+import { Source_Sans_Pro } from '@next/font/google';
+import { Layout } from 'components';
+import 'tailwindcss/tailwind.css';
+import 'styles/globals.css';
 
 const sans = Source_Sans_Pro({
-  weight: ["200", "300", "400", "600", "700", "900"],
-  style: ["italic", "normal"],
+  weight: ['200', '300', '400', '600', '700', '900'],
+  style: ['italic', 'normal'],
   preload: true,
-  subsets: ["latin"],
+  subsets: ['latin'],
 });
 
 export default function App({ Component, pageProps }: AppProps) {
