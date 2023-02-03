fd8589cb222274a121970adb4994801705248669
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-02 22:49:02 -0600

    add home page details

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import { Article, Anchor, Separator } from '@/components';
+import { FaGithub, FaLinkedin } from 'react-icons/fa';
+import { Article, Anchor, IconButtonLink } from '@/components';
 
 export default function Home() {
   return (
@@ -15,39 +16,59 @@ export default function Home() {
         />
         <link rel="icon" href="/ui.ico" />
       </Head>
-      <main className="p-9 w-full min-h-screen m-auto flex flex-col items-center justify-start">
-        <Article className="w-[50%]">
+      <main className="p-9 w-full h-auto m-auto flex flex-col items-center justify-start">
+        <Article className="w-full desktop:w-[50%]">
           <h1 className="text-4xl font-bold text-center">
             Speed up your development process using highly customizable react
             components.
           </h1>
           <br />
-          <p className="text-[20px] leading-9 italic text-justify">
+          <p className="text-[20px] leading-9 text-justify">
             Facile UI is a components library, built in top of React, using
             typescript and tailwind css. Its main purpose is to speed up
             development process by providing templates for common components,
             all of which can be customized to project requirements. <br />
           </p>
-          <div className="w-[30%] flex flex-col items-center justify-around h-auto gap-3 m-auto mt-4">
+          <div className="w-fit flex items-center justify-center flex-col h-auto gap-3 m-auto mt-10">
             <Anchor
-              className="!w-full px-8 font-semibold"
+              className="px-8 !w-full font-semibold"
               href="/get-started"
               variant="primary"
             >
               Get started
             </Anchor>
             <Anchor
-              className="!w-full px-8 font-semibold"
+              className="px-8 font-semibold"
               href="/get-started"
               variant="secondary"
             >
               Components overview
             </Anchor>
           </div>
-          <p className="text-[18px] leading-9 italic text-center mt-6">
+          <p className="text-[18px] leading-8 m-auto mt-10">
+            Reach out on linkedin if you have any comments or suggestions. Also,
+            if you want to collaborate or get a closer look on how the
+            components work you can overview the project repositorie.
+          </p>
+          <div className="w-fit flex items-center justify-center gap-3 m-auto mt-6">
+            <IconButtonLink
+              href="https://github.com/Amaury-Vasquez/facile-ui"
+              target="_blank"
+              Icon={FaGithub}
+              className="h-8 w-8"
+            />
+            <IconButtonLink
+              href="https://www.linkedin.com/in/amaury-vasquez-9a1b3a240/"
+              target="_blank"
+              Icon={FaLinkedin}
+              iconColor="#0a66c2"
+              className="h-8 w-8"
+            />
+          </div>
+          <p className="text-[16px] leading-9 text-center mt-6">
             Created and mantained by{' '}
             <Anchor
-              className="text-[18px] italic"
+              className="text-[16px] italic semibold"
               href="https://github.com/Amaury-Vasquez"
             >
               @amvasdev
