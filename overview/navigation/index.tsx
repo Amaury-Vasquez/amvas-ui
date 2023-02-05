1526f5510e1a9a3dfe4c82f139336353752ac912
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:57:42 -0600

    comment unused components

--- overview/navigation/index.tsx
+++ overview/navigation/index.tsx
@@ -1,23 +1,23 @@
-import { ComponentSection } from '@/overview';
-
-export const navigationSection: ComponentSection = {
-  classification: 'navigation',
-  components: [
-    {
-      name: 'anchor',
-      path: 'anchor',
-    },
-    {
-      name: 'breadcrumb',
-      path: 'breadcrumb',
-    },
-    {
-      name: 'navigation link',
-      path: 'navigation-link',
-    },
-    {
-      name: 'navigation menu',
-      path: 'navigation-menu',
-    },
-  ],
-};
+// import { ComponentSection } from '@/overview';
+export {};
+// export const navigationSection: ComponentSection = {
+//   classification: 'navigation',
+//   components: [
+//     {
+//       name: 'anchor',
+//       path: 'anchor',
+//     },
+//     {
+//       name: 'breadcrumb',
+//       path: 'breadcrumb',
+//     },
+//     {
+//       name: 'navigation link',
+//       path: 'navigation-link',
+//     },
+//     {
+//       name: 'navigation menu',
+//       path: 'navigation-menu',
+//     },
+//   ],
+// };
