eaae1e9770859102eecd5b25b2b5539099a064ff
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:58:02 -0600

    remove meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,10 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta
-          name="description"
-          content="Facile UI, react and nextjs components library, built with tailwind css"
-        />
         <link rel="icon" href="/ui.ico" />
       </Head>
       <body>
