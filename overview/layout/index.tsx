0bc66c72fc12617f425f621b8b40e6f840842635
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:56:56 -0600

    comment unused components

--- overview/layout/index.tsx
+++ overview/layout/index.tsx
@@ -1,35 +1,36 @@
-import { ComponentSection } from '@/overview';
+import { FC } from 'react';
 
-export const layoutSection: ComponentSection = {
-  classification: 'layout',
-  components: [
-    // {
-    //   name: 'divider',
-    //   path: 'divider',
-    // },
-    {
-      name: 'grid',
-      path: 'grid',
-    },
-    {
-      name: 'footer',
-      path: 'footer',
-    },
-    {
-      name: 'header',
-      path: 'header',
-    },
-    {
-      name: 'layout (header)',
-      path: 'header-layout',
-    },
-    {
-      name: 'layout (sider)',
-      path: 'sider-layout',
-    },
-    {
-      name: 'sider',
-      path: 'sider',
-    },
-  ],
-};
+// import { ComponentSection } from '@/overview';
+// export const layoutSection: ComponentSection = {
+//   classification: 'layout',
+//   components: [
+//     // {
+//     //   name: 'divider',
+//     //   path: 'divider',
+//     // },
+//     {
+//       name: 'grid',
+//       path: 'grid',
+//     },
+//     {
+//       name: 'footer',
+//       path: 'footer',
+//     },
+//     {
+//       name: 'header',
+//       path: 'header',
+//     },
+//     {
+//       name: 'layout (header)',
+//       path: 'header-layout',
+//     },
+//     {
+//       name: 'layout (sider)',
+//       path: 'sider-layout',
+//     },
+//     {
+//       name: 'sider',
+//       path: 'sider',
+//     },
+//   ],
+// };
