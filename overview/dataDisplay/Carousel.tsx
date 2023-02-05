17c60b04df9750217beb1ec0e1f74e3160c26680
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:58:33 -0600

    delete components

--- overview/dataDisplay/Carousel.tsx
+++
@@ -1,9 +0,0 @@
-import { Carousel } from '@/components';
-
-const images: string[] = [
-  'https://i.imgur.com/RTKfmvx.png',
-  'https://i.imgur.com/PBdg8dB.png',
-  'https://i.imgur.com/r9uzjUf.png',
-];
-
-export const CarouselOverview = () => <Carousel images={images} />;
