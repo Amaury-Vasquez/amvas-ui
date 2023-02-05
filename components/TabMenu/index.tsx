873a470fe1152c4ca7e9d24fa5f2b7cc0d2f7121
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:50:21 -0600

    remove return statement

--- components/TabMenu/index.tsx
+++ components/TabMenu/index.tsx
@@ -28,24 +28,22 @@ export interface TabMenuProps {
   items: IconTabProps[] | TextTabProps[];
 }
 
-const TabMenu: FC<TabMenuProps> = ({ className, items, rounded = false }) => {
-  return (
-    <ul
-      className={clsx(
-        'p-0 m-0 flex gap-3 w-fit h-12 py-1 bg-neutral-100',
-        rounded ? 'rounded-full px-6' : 'px-4 rounded-md',
-        className
-      )}
-    >
-      {items.map((tab, i) =>
-        tab.Icon ? (
-          <IconTab key={'icontab' + i + tab.Icon.toString()} {...tab} />
-        ) : (
-          <TextTab key={'texttab' + i + tab.name} {...tab} />
-        )
-      )}
-    </ul>
-  );
-};
+const TabMenu: FC<TabMenuProps> = ({ className, items, rounded = false }) => (
+  <ul
+    className={clsx(
+      'p-0 m-0 flex gap-3 w-fit h-12 py-1 bg-neutral-100',
+      rounded ? 'rounded-full px-6' : 'px-4 rounded-md',
+      className
+    )}
+  >
+    {items.map((tab, i) =>
+      tab.Icon ? (
+        <IconTab key={'icontab' + i + tab.Icon.toString()} {...tab} />
+      ) : (
+        <TextTab key={'texttab' + i + tab.name} {...tab} />
+      )
+    )}
+  </ul>
+);
 
 export default TabMenu;
