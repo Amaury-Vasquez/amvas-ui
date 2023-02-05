773e98e337cd29f70b936f18926c36c9b2fcfc0e
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:56:07 -0600

    add type to component

--- overview/general/Button.tsx
+++ overview/general/Button.tsx
@@ -1,6 +1,7 @@
+import { FC } from 'react';
 import { Button } from '@/components';
 
-export const ButtonOverview = () => (
+export const ButtonOverview: FC = () => (
   <div className="p-4 grid grid-cols-2 gap-4 w-full h-full items-center justify-center">
     <Button variant="primary">Primary</Button>
     <Button variant="secondary">Secondary</Button>
