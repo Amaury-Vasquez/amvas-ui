2951ff32e47dd7c3383bdc18648d2791d1df83cc
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:57:12 -0600

    comment unused components

--- overview/loaders/index.tsx
+++ overview/loaders/index.tsx
@@ -1,19 +1,20 @@
-import { ComponentSection } from '@/overview';
+export { SpinOverview } from './Spin';
 
-export const loadersSection: ComponentSection = {
-  classification: 'loaders',
-  components: [
-    {
-      name: 'bounce',
-      path: 'bounce',
-    },
-    {
-      name: 'skeleton',
-      path: 'skeleton',
-    },
-    {
-      name: 'spin',
-      path: 'spin',
-    },
-  ],
-};
+// export const loadersSection: ComponentSection = {
+//   classification: 'loaders',
+//   components: [
+//     {
+//       name: 'bounce',
+//       path: 'bounce',
+//     },
+//     {
+//       name: 'skeleton',
+//       path: 'skeleton',
+//     },
+//     {
+//       name: 'spin',
+//       path: 'spin',
+//       Component: SpinOverview,
+//     },
+//   ],
+// };
