f1238085074950947fd93a6f3afae5bd4ffc3404
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-04 18:24:43 -0600

    remove prop from overview component

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,10 +1,8 @@
 import { NextPage } from 'next';
-import { useComponentList } from '@/hooks';
 import Overview from '@/overview';
 
 const Components: NextPage = () => {
-  const sections = useComponentList();
-  return <Overview sections={sections} />;
+  return <Overview />;
 };
 
 export default Components;
