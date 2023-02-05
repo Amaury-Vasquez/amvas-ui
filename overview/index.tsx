66cb94607034abdce881c5b301d4050f3e89cfea
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-05 13:51:40 -0600

    migrate interfaces to componentlist hook, export sections array

--- overview/index.tsx
+++ overview/index.tsx
@@ -1,32 +1,9 @@
-import { FC } from 'react';
-import Overview from './Overview';
-import { generalSection } from './general';
-import { layoutSection } from './layout';
-import { dataDisplaySection } from './dataDisplay';
-import { dataInputSection } from './dataInput';
-import { feedbackSection } from './feedback';
-import { navigationSection } from './navigation';
-import { loadersSection } from './loaders';
+export { default as Overview } from './Overview';
 
-interface ComponentInfo {
-  name: string;
-  path: string;
-  Component?: FC<any>;
-}
-
-export interface ComponentSection {
-  classification: string;
-  components: ComponentInfo[];
-}
-
-export const componentList: ComponentSection[] = [
-  generalSection,
-  layoutSection,
-  dataDisplaySection,
-  dataInputSection,
-  feedbackSection,
-  navigationSection,
-  loadersSection,
-];
-
-export default Overview;
+export { generalSection } from './general';
+export { dataDisplaySection } from './dataDisplay';
+// import { layoutSection } from './layout';
+// import { dataInputSection } from './dataInput';
+// import { feedbackSection } from './feedback';
+// import { navigationSection } from './navigation';
+// import { loadersSection } from './loaders';
