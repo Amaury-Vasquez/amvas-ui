bf0abda14f9c51af1449c74851eb14ee0b6e4159
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-04 18:23:34 -0600

    create overview component

---
+++ overview/Overview.tsx
@@ -0,0 +1,40 @@
+import { FC } from 'react';
+import { capitalize } from '@/utils/string';
+import { Card, Separator } from '@/components';
+import { componentList } from '.';
+
+const Overview: FC = () => {
+  const renderSections = () =>
+    componentList.map((section, i) => {
+      const { classification } = section;
+      return (
+        <div
+          className="w-full h-auto flex flex-col gap-2"
+          key={`parentsection${classification}`}
+        >
+          <span className="text-xl font-semibold text-neutral-900">
+            {capitalize(classification)}
+          </span>
+          <Separator />
+          <div className="w-full h-auto grid grid-cols-5 items-center justify-center gap-2">
+            {section.components.map((component, j) =>
+              component.Component ? (
+                <Card
+                  title={capitalize(component.name)}
+                  href={`/components/${classification.replace(' ', '-')}/${
+                    component.path
+                  }`}
+                  key={classification + component.name + i + j}
+                  content={<component.Component />}
+                />
+              ) : null
+            )}
+          </div>
+        </div>
+      );
+    });
+
+  return <div className="flex flex-col gap-4">{renderSections()}</div>;
+};
+
+export default Overview;
