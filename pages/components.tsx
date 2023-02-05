eb4b2712d9fbd364680fa9b5fb353c88141f6d18
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-04 18:31:24 -0600

    add Head to page

--- pages/components.tsx
+++ pages/components.tsx
@@ -1,8 +1,21 @@
 import { NextPage } from 'next';
+import Head from 'next/head';
 import Overview from '@/overview';
 
 const Components: NextPage = () => {
-  return <Overview />;
+  return (
+    <>
+      <Head>
+        <title> Facile UI - Components overview </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          name="description"
+          content="Facile UI components overview, general, data display, data input, feedback, navigation, loaders"
+        />
+      </Head>
+      <Overview />
+    </>
+  );
 };
 
 export default Components;
