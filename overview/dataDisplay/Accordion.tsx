2257f2a6cb27ffdfa00e73bd4ef54796f44ffb00
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-04 22:16:44 -0600

    edit style of sections

--- overview/dataDisplay/Accordion.tsx
+++ overview/dataDisplay/Accordion.tsx
@@ -8,13 +8,13 @@ export const AccordionOverview: FC = () => (
     bordered
     toggleableContent={
       <div className="flex flex-col text-center gap-1 text-violet-800">
-        <span className="text-base italic border-b border-solid border-violet-600 ">
+        <span className="text-base italic border-b border-solid border-violet-600 py-1">
           Section 1
         </span>
-        <span className="text-base italic border-b border-solid border-violet-600">
+        <span className="text-base italic border-b border-solid border-violet-600 py-1">
           Section 2
         </span>
-        <span className="text-base italic"> Section 3 </span>
+        <span className="text-base italic py-1"> Section 3 </span>
       </div>
     }
   />
