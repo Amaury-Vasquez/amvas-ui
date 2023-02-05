be41ce17e02a33468e9eeb969c3831c3766a9233
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-04 22:14:48 -0600

    export avatarm carousel and tabs

--- components/index.tsx
+++ components/index.tsx
@@ -1,8 +1,10 @@
 export { default as Accordion } from './Accordion';
 export { default as Article } from './Article';
 export { default as Anchor } from './Anchor';
+export { default as Avatar } from './Avatar';
 export { default as Button } from './Button';
 export { default as Card } from './Card';
+export { default as Carousel } from './Carousel';
 export * from './IconButton';
 export { default as Layout } from './Layout';
 export {
@@ -12,3 +14,11 @@ export {
 export { default as NavigationMenu } from './NavigationMenu';
 export { default as Separator } from './Separator';
 export { default as Sider } from './Sider';
+export {
+  default as TabMenu,
+  TextTab,
+  IconTab,
+  type TabCommonProps,
+  type IconTabProps,
+  type TextTabProps,
+} from './TabMenu';
