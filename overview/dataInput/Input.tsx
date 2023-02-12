3429bbdfac198f09d3dee16857630d5c27ea78ff
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-12 11:29:27 -0600

    add readonly prop to inputs

--- overview/dataInput/Input.tsx
+++ overview/dataInput/Input.tsx
@@ -5,9 +5,15 @@ import { TextInput } from '@/components';
 
 const InputOverview: FC = () => (
   <div className="flex flex-col w-full h-full gap-4 justify-center">
-    <TextInput placeholder="Text input" />
-    <TextInput type="email" placeholder="Icon input" Icon={AiOutlineMail} />
+    <TextInput readOnly placeholder="Text input" />
     <TextInput
+      readOnly
+      type="email"
+      placeholder="Icon input"
+      Icon={AiOutlineMail}
+    />
+    <TextInput
+      readOnly
       type="password"
       value="howareyoudude?"
       Icon={RiLockPasswordLine}
