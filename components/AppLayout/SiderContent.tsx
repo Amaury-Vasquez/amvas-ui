dc0395f80029b957e279fd6d87d30d85f9b76ca7
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-12 11:24:41 -0600

    create component for sider content

---
+++ components/AppLayout/SiderContent.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react';
+import { useComponentList } from '@/hooks';
+import { Accordion, NavigationLink, NavigationMenu } from '@/components';
+
+const SiderContent: FC = () => {
+  const { navigationList } = useComponentList();
+  const linkClass = 'w-full px-4 py-2 bg-violet-700 text-base font-semibold';
+  return (
+    <div className="transition-[height] duration-1000 ease h-auto w-auto p-0 m-0">
+      <NavigationLink
+        className={linkClass}
+        text="Get started"
+        href="/get-started"
+      />
+      <NavigationLink
+        className={linkClass}
+        text="Components"
+        href="/components"
+      />
+      {navigationList.map((section, i) => (
+        <Accordion
+          text={section.classification}
+          key={`nav-accordion-${section.classification}${i}`}
+          toggleableContent={
+            <NavigationMenu
+              links={section.components.map((component) => ({
+                text: component.name,
+                href: component.path,
+              }))}
+            />
+          }
+        />
+      ))}
+    </div>
+  );
+};
+
+export default SiderContent;
