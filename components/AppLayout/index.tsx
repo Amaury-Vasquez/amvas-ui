b47c8ceb153e72e83627a779ded069ad4bfc7d2b
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-12 11:22:10 -0600

    create app layout component

---
+++ components/AppLayout/index.tsx
@@ -0,0 +1,79 @@
+import clsx from 'clsx';
+import { useRouter } from 'next/router';
+import { NextFont } from '@next/font';
+import { FC } from 'react';
+import { FaReact } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+import HeaderLinks from './HeaderLinks';
+import Menu from './Menu';
+import SiderContent from './SiderContent';
+import { Header, IconButton, Modal, NavigationLink, Sider } from '@/components';
+import { useToggle } from '@/hooks';
+import { capitalize } from '@/utils/string';
+
+interface LayoutProps {
+  children: React.ReactNode;
+  font: NextFont;
+}
+
+const AppLayout: FC<LayoutProps> = ({ children, font }) => {
+  const { isActive, willClose, handleClick } = useToggle(false, 280);
+  const router = useRouter();
+  const handleBlur = () => {
+    if (isActive) handleClick();
+  };
+
+  return (
+    <div
+      className={clsx(
+        'w-screen min-h-screen h-auto overflow-y-auto animate-fade-in-05 bg-slate-100',
+        font.className
+      )}
+    >
+      <Header
+        left={<Menu isActive={isActive} onMenuClick={handleClick} />}
+        middle={
+          <span className="flex items-center justify-center w-fit h-full text-xl text-neutral-800 font-semibold">
+            {router.pathname === '/'
+              ? 'Facile UI'
+              : capitalize(
+                  router.pathname.replace('/', '').split('-').join(' ')
+                )}
+          </span>
+        }
+        right={<HeaderLinks />}
+      />
+      {isActive && (
+        <Modal className={clsx(willClose && 'animate-fade-out')}>
+          <Sider
+            className={clsx(
+              'animate-appear-right origin-left',
+              willClose && 'animate-dissapear-left origin-left'
+            )}
+            content={<SiderContent />}
+            title={
+              <div className="w-full flex items-center justify-between h-auto mb-2 px-4">
+                <NavigationLink
+                  href={'/'}
+                  text="Facile UI"
+                  Icon={FaReact}
+                  className="text-xl font-semibold"
+                />
+                <IconButton
+                  Icon={AiOutlineClose}
+                  hoverColor="bg-violet-200"
+                  iconColor="#fff"
+                  onClick={handleClick}
+                />
+              </div>
+            }
+            handleClose={handleBlur}
+          />
+        </Modal>
+      )}
+      <div className={clsx('p-6 w-full mt-12')}>{children}</div>
+    </div>
+  );
+};
+
+export default AppLayout;
