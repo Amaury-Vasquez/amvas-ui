d80bda61e3f74c8f79441f8a482053937d984dee
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-18 11:11:56 -0600

    create bounce overview component

---
+++ overview/loaders/Bounce.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react';
+import { Bounce } from '@/components';
+
+const BounceOverview: FC = () => (
+  <div className="flex w-full h-full flex-col items-center justify-center gap-8">
+    <div className="flex gap-1 items-center justify-center">
+      <Bounce />
+      <Bounce size="md" />
+      <Bounce size="lg" />
+    </div>
+    <Bounce size="lg" backgroundColor="violet-500" />
+  </div>
+);
+
+export default BounceOverview;
