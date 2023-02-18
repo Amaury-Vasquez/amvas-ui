55422299ec4d10d6df61ee980fa3081dd421940c
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-18 11:09:34 -0600

    change spinner style

--- components/Loaders/Spin.tsx
+++ components/Loaders/Spin.tsx
@@ -1,29 +1,25 @@
 import { FC } from 'react';
 import clsx from 'clsx';
+import styles from '@/styles/loaders.module.css';
 
 interface SpinProps {
-  size?: 'xs' | 'sm' | 'md' | 'lg';
   color?: string;
-  speed?: 'avg' | 'fast';
   className?: string;
 }
 
-const Spin: FC<SpinProps> = ({
-  size = 'md',
-  color = '#7a44e8',
-  speed = 'avg',
-  className,
-}) => (
+const Spin: FC<SpinProps> = ({ color = 'blue-500', className }) => (
   <div
     className={clsx(
-      size === 'xs' && 'w-6 h-6',
-      size === 'sm' && 'w-8 h-8',
-      size === 'md' && 'w-10 h-10',
-      size === 'lg' && 'w-12 h-12',
-      `bg-[${color}] animate-spin-${speed} rounded-full`,
+      styles.ldsRing,
+      'border-transparent',
+      color ? `border-t-${color}` : 'border-t-blue-500',
       className
     )}
-  ></div>
+  >
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
 );
 
 export default Spin;
