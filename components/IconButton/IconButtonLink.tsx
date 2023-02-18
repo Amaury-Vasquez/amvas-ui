623668835219c96be1baae0e9185917a22c2ab16
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-18 11:06:21 -0600

    add negative index for tab navigation omit

--- components/IconButton/IconButtonLink.tsx
+++ components/IconButton/IconButtonLink.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
 import Link, { LinkProps } from 'next/link';
+import { TargetTypes } from '@/interfaces';
 import IconButton from './IconButton';
 import { IconCommonProps } from '.';
 
 interface IconButtonLinkProps extends LinkProps, IconCommonProps {
-  target?: string;
+  target?: TargetTypes;
 }
 
 const IconButtonLink: FC<IconButtonLinkProps> = ({
@@ -15,8 +16,13 @@ const IconButtonLink: FC<IconButtonLinkProps> = ({
   ...props
 }) => {
   return (
-    <Link target={target} {...props}>
-      <IconButton Icon={Icon} iconColor={iconColor} className={className} />
+    <Link className="cursor-pointer rounded-full" target={target} {...props}>
+      <IconButton
+        tabIndex={-1}
+        Icon={Icon}
+        iconColor={iconColor}
+        className={className}
+      />
     </Link>
   );
 };
