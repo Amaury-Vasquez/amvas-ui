aea3c4a16d18bb2c2523e6b2d3af437828c08728
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-02-18 11:12:59 -0600

    remove viewport meta tag

--- pages/components.tsx
+++ pages/components.tsx
@@ -9,7 +9,6 @@ export default function Components() {
     <>
       <Head>
         <title> Facile UI - Components overview </title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           name="description"
           content="Facile UI components overview, general, data display, data input, feedback, navigation, loaders"
