5012cb6a7f64d51304bbe1cb5ef1d29b6871a69c
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-03-03 21:47:30 -0600

    change accordion style and add disabled prop

--- overview/dataDisplay/Accordion.tsx
+++ overview/dataDisplay/Accordion.tsx
@@ -3,20 +3,24 @@ import clsx from 'clsx';
 import { Accordion } from '@/components';
 
 export const AccordionOverview: FC = () => {
-  const commonClasses = 'border-solid border-neutral-300 text-slate-800';
+  const commonClasses =
+    'border-solid border-neutral-300 text-slate-800 cursor-pointer';
   const spanClasses = 'w-full flex items-center justify-center py-1.5';
   const content = (
     <div className={clsx(commonClasses, 'border-t flex flex-col')}>
-      <span className={clsx(commonClasses, spanClasses, 'border-b')}>
+      <button disabled className={clsx(commonClasses, spanClasses, 'border-b')}>
         Option 1
-      </span>
-      <span className={clsx(commonClasses, spanClasses)}> Option 2</span>
+      </button>
+      <button disabled className={clsx(commonClasses, spanClasses)}>
+        Option 2
+      </button>
     </div>
   );
   return (
     <div className="w-48 h-fit flex items-center justify-center">
       <Accordion
         active
+        disabled
         className={clsx(commonClasses, 'border rounded-md')}
         text="Accordion"
         toggleableContent={content}
