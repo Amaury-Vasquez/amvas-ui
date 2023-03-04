e702f6117fa6ae19149e6d238d33fa5ff966fd81
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-03-03 23:16:52 -0600

    export button types

--- components/index.tsx
+++ components/index.tsx
@@ -4,7 +4,7 @@ export { default as Accordion } from './Accordion';
 export * from './Article';
 export { default as Anchor } from './Anchor';
 export { default as Avatar } from './Avatar';
-export { default as Button } from './Button';
+export { default as Button, type ButtonTypes } from './Button';
 export { default as Card } from './Card';
 export { default as Carousel } from './Carousel';
 export { default as CodeBlock } from './CodeBlock';
