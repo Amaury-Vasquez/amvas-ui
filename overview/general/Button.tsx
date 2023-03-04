4666f247c3936d7f2c93ccefd900413742020fa9
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-03-03 21:47:57 -0600

    change error (primary dark)

--- overview/general/Button.tsx
+++ overview/general/Button.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components';
 export const ButtonOverview: FC = () => (
   <div className="p-4 grid grid-cols-2 gap-4 w-full h-full items-center justify-center">
     <Button variant="primary">Primary</Button>
-    <Button variant="primary-dark">primary-dark</Button>
+    <Button variant="secondary">Secondary</Button>
     <Button variant="base">Base</Button>
     <Button variant="base" underlined>
       Underlined
