43dc4454bd8f76b2aefc7771fd945e68f9db5e81
Amaury Vasquez <amaury.vasquezenriquez@gmail.com>
2023-03-12 13:10:22 -0600

    add lazy loading

--- components/CodeBlock/index.tsx
+++ components/CodeBlock/index.tsx
@@ -1,23 +1,30 @@
+import dynamic from 'next/dynamic';
 import { FC } from 'react';
-import SyntaxHiglighter from 'react-syntax-highlighter';
 import { atomOneLight } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { Skeleton } from '../Loaders';
 import { concatStringLines } from '@/utils';
 
 interface CodeBlockProps {
   code: string[];
 }
 
+const SyntaxHighlighter = dynamic(() => import('react-syntax-highlighter'), {
+  ssr: false,
+  loading: () => <Skeleton className="w-full h-48" />,
+});
+
 const CodeBlock: FC<CodeBlockProps> = ({ code }) => (
-  <SyntaxHiglighter
+  <SyntaxHighlighter
     customStyle={{
       borderRadius: '2px',
       padding: '16px',
       position: 'relative',
+      animation: 'fade-in 1s ease-in-out',
     }}
     language="tsx"
     style={atomOneLight}
   >
     {concatStringLines(code)}
-  </SyntaxHiglighter>
+  </SyntaxHighlighter>
 );
 export default CodeBlock;
